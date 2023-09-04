735dc9b8dffca8c13331495636074a84b8f789b7
slotZero01 <slotzero@native.org>
2023-09-04 18:00:58 +0800

    init umd and vuejs integration

---
+++ vuejs/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <NativeSwapWidget apiKey="JWL73SF2K899AMPFRHZV" />
+  </main>
+</template>
+
+<script>
+import { applyPureReactInVue } from "veaury";
+import NativeSwapWidget from "@native_org/widgets";
+
+export default {
+  components: { NativeSwapWidget: applyPureReactInVue(NativeSwapWidget) },
+};
+</script>
+
+<style scoped></style>
---
+++ vuejs/vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import veauryVitePlugins from 'veaury/vite/index.js'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // vue(),
+    // vueJsx(),
+    veauryVitePlugins({
+      type: 'vue'
+      // Configuration of @vitejs/plugin-vue
+      // vueOptions: {...},
+      // Configuration of @vitejs/plugin-react
+      // reactOptions: {...},
+      // Configuration of @vitejs/plugin-vue-jsx
+      // vueJsxOptions: {...}
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
