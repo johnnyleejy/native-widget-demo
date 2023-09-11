021a688f24d79f5f8e58f92930826ddf0e3634f2
slotZero01 <slotzero@native.org>
2023-09-11 15:45:57 +0800

    init all

---
+++ react18/src/App.js
@@ -0,0 +1,12 @@
+import SwapWidget from "@native_org/widgets";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SwapWidget apiKey="8Y0ZN9X6VF7VW0KMF3P4" />
+    </div>
+  );
+}
+
+export default App;
